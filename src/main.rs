04ee0478745aa1b567c483f3db4dc60363a13ea4
Tyler Oalman <tyleroalman@gmail.com>
2021-08-23 17:28:46 -0500

    Print the `PanicInfo` on panic

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
